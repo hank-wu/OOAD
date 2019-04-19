b91472b59a31fd289b227b65c0d2859b11d9e8c6
angularjs <angular@example.com>
2019-04-19 18:44:33 +0800

    add some

--- RMS/test/rms_test/test_usecase/test_show_seat_order.h
+++ RMS/test/rms_test/test_usecase/test_show_seat_order.h
@@ -1,4 +1,15 @@
 #ifndef TEST_SHOW_SEAT_ORDER_H
 #define TEST_SHOW_SEAT_ORDER_H
 
+#include "../../../usecase/manage_order.h"
+
+#include <gtest/gtest.h>
+#include <gmock/gmock-matchers.h>
+
+
+TEST(TestManageOrder, first)
+{
+    ManageOrder * m = new ManageOrder();
+    ASSERT_EQ(10,m->getSeatCount());
+}
 #endif // TEST_SHOW_SEAT_ORDER_H
